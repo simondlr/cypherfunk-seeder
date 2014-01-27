57b66c478782d17df309313b594f19c30658d803
langerhans <max.keller@gmx.com>
2014-01-27 18:51:33 +0100

    Conversion to Dogecoin

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70002
+#define REQUIRE_VERSION 60003 //TODO: Should be upped, when adoption of newer version is high enough.
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 470000;
+    return testnet ? 0 : 60000;
 }
 
 std::string static inline ToString(const CService &ip) {
--- main.cpp
+++ main.cpp
@@ -31,7 +31,7 @@ public:
   CDnsSeedOpts() : nThreads(96), nDnsThreads(4), nPort(53), mbox(NULL), ns(NULL), host(NULL), tor(NULL), fUseTestNet(false), fWipeBan(false), fWipeIgnore(false) {}
 
   void ParseCommandLine(int argc, char **argv) {
-    static const char *help = "Litecoin-seeder\n"
+    static const char *help = "Dogecoin-seeder\n"
                               "Usage: %s -h <host> -n <ns> [-m <mbox>] [-t <threads>] [-p <port>]\n"
                               "\n"
                               "Options:\n"
@@ -342,14 +342,16 @@ extern "C" void* ThreadStats(void*) {
   } while(1);
 }
 
-static const string mainnet_seeds[] = {"dnsseed.litecointools.com", "dnsseed.litecoinpool.org", "dnsseed.ltc.xurious.com", ""};
-static const string testnet_seeds[] = {"testnet-seed.litecointools.com", ""};
+//TODO: We don't have any other seeds yet. Add them in next revision.
+static const string mainnet_seeds[] = {""};
+static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
-  if (!fTestNet){
-    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 9333), true);
-  }
+//TODO: No .onion seed yet.
+//  if (!fTestNet){
+//    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 9333), true);
+//  }
   do {
     for (int i=0; seeds[i] != ""; i++) {
       vector<CNetAddr> ips;
@@ -377,10 +379,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0xfc;
-      pchMessageStart[1] = 0xc1;
-      pchMessageStart[2] = 0xb7;
-      pchMessageStart[3] = 0xdc;
+      pchMessageStart[0] = 0xc0;
+      pchMessageStart[1] = 0xc0;
+      pchMessageStart[2] = 0xc0;
+      pchMessageStart[3] = 0xc0;
       seeds = testnet_seeds;
       fTestNet = true;
   }
